d7fe7a86608c7e37a3afde9467f4feb386516281
Colman Mwakio <cmwakio@gmail.com>
2020-01-24 03:16:56 +0300

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import Joke from './components/Joke';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/" exact component={Joke} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>This is a Dads Joke App</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iusto ipsam, facilis, accusantium debitis laborum cupiditate eveniet sunt vitae, voluptatibus amet ratione ad quidem temporibus non error? Voluptate, beatae et.</p>
+    </div>
+  )
+}
